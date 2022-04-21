b80667ce3db5f31304478bdc054847933c3e6564
Or Hasson <hassonor@gmail.com>
2022-04-21 09:42:31 +0300

    remove unused import

--- rn-app-using-expo/my-app/App.js
+++ rn-app-using-expo/my-app/App.js
@@ -1,5 +1,4 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from "@react-navigation/stack";
 import HomePage from './Home';
